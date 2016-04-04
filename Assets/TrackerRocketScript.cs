3b4a9ed76f85b5ce4b6fa519540d2b90ba4c2892
ShooterArk <smallassasin@gmail.com>
2016-04-04 14:51:48 +0500

    added some more

---
+++ Assets/TrackerRocketScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackerRocketScript : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 10f;
+	Vector3 screenPos;
+
+	// Use this for initialization
+	void Start () {
+		//screenPos  = Camera.main.WorldToScreenPoint (target.position);
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target != null){
+			transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+		}
+	}
+
+	/*void OnCollisionEnter2D(Collision2D col)
+	{
+		
+		if (col.gameObject.name == "Platform") {
+			Destroy(gameObject);
+		}
+		
+		if (col.gameObject.tag == "Player") {
+			GameObject gcc = GameObject.FindGameObjectWithTag("GameController");
+			GameController gc = gcc.GetComponent<GameController>();
+			gc.GameOver();
+			Destroy(col.gameObject);
+		}
+		
+	}*/
+
+}
---
+++ Assets/TrackingScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingScript : MonoBehaviour {
+
+	public Transform target;
+	public Texture2D trackIcon;
+	
+	void Awake(){
+		
+	}
+	
+	void Start(){
+		
+	}
+	
+	void Update () {
+		if(target != null)
+		{
+			Vector3 screenPos  = Camera.main.WorldToScreenPoint (target.position);
+		}
+	}
+	
+	void OnGUI(){
+		if(target != null)
+		{
+			Vector3 screenPos  = Camera.main.WorldToScreenPoint (target.position);
+			float reverse = Screen.height;
+			var clampX = Mathf.Clamp(screenPos.x, 0, Screen.width);
+			var clampY = Mathf.Clamp(reverse - screenPos.y, 0, Screen.height);
+			
+			//GUI.DrawTexture(new Rect(clampX, clampY, trackIcon.width / 2, trackIcon.height / 2), trackIcon);
+		}
+	}
+
+}
